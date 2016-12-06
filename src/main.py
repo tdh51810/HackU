ae040eb8ef3174c0a4bad36d59cfa6b3b38fc458
tnk55397 <tnk55397@st.yamagata-u.ac.jp>
2016-12-06 14:10:13 +0900

    get_worning.pyのreturnの変更に合わせて修正

--- src/main.py
+++ src/main.py
@@ -2,26 +2,33 @@
 import random
 import get_warning
 import postSlack
+import file_func
 
-# ユーザ情報を読み込む
-userdata = eval(open("../data/user_profile/user_info.txt","r").read())
+while True:
+    while True:
+        if file_func.get_trig() = True:
+            file_func.get_prof()
+            break
 
-# 理由テキストの生成に成功するまでループする
-text = None
-while(text is None):
-    # 理由生成アルゴリズムをランダムに選択
-    algorithm = random.randint(1, 2)
-    # 注意報・警報データを用いた理由生成
-    if(algorithm == 1):
-        # 注意報・警報データを取得し、存在するならテキストを生成
-        text = get_warning.get_warning(userdata)
-
-    # 定型文による理由生成(仮)
-    elif(algorithm == 2):
-        text = "風邪を引いたので休みます。"
-
-# 整形してSlackに投稿
-postSlack.post_RestMessage(text, userdata)
-
-# Botが休み・遅刻を肯定するようなメッセージを投稿
-postSlack.post_BotMessage(rest_flag, userdata) # rest_flagはbttnが押された時に生成されるファイルの中身（青（遅刻）:0休み：それ以外）
+    # ユーザ情報を読み込む
+    userdata = eval(open("../data/user_profile/user_info.txt","r").read())
+    
+    # 理由テキストの生成に成功するまでループする
+    text = None
+    while(text is None):
+        # 理由生成アルゴリズムをランダムに選択
+        algorithm = random.randint(1, 2)
+        # 注意報・警報データを用いた理由生成
+        if(algorithm == 1):
+            # 注意報・警報データを取得し、存在するならテキストを生成
+            text = get_warning.get_warning(userdata)
+    
+        # 定型文による理由生成(仮)
+        elif(algorithm == 2):
+            text = "風邪を引いたので休みます。"
+    
+    # 整形してSlackに投稿
+    postSlack.post_RestMessage(text, userdata)
+    
+    # Botが休み・遅刻を肯定するようなメッセージを投稿
+    postSlack.post_BotMessage(rest_flag, userdata) # rest_flagはbttnが押された時に生成されるファイルの中身（青（遅刻）:0休み：それ以外）
