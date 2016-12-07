3b9c7f8eafb18c2a14f19357b6b067a0aafb452a
tey26595 <tey26595@st.yamagata-u.ac.jp>
2016-12-07 12:40:53 +0900

    ネタツイートをランダムに抽出

---
+++ src/netatwi.py
@@ -0,0 +1,16 @@
+import random
+
+def neta():
+
+	tweet = open("reason_from_twitter.txt","r").read()
+	tweetlist = tweet.split("\n")
+
+  #空要素をリストから削除
+	while(True):
+		try:
+			tweetlist.remove("")
+		except ValueError:
+			break
+
+	one_tweet = random.choice(tweetlist)
+	return one_tweet
