5d2722b98b43294afb29ff9335f05264f235c4ee
tey26595 <tey26595@st.yamagata-u.ac.jp>
2016-12-07 12:42:24 +0900

    遠い親戚からランダムに倒す

---
+++ src/sinseki.py
@@ -0,0 +1,113 @@
+import random
+
+def kill():
+	userdata = eval(open("user_info.txt","r").read())
+	userdata2 = open("user_info.txt","w")
+	kill_list = ["父親","母親","祖父","祖母","兄","姉","弟","妹","叔父","叔母","いとこ"]
+
+#いとこから優先して◯す
+	if int(userdata["いとこ"]) >= 1 :
+		userdata["いとこ"] = int(userdata["いとこ"]) - 1
+		userdata["いとこ"] = str(userdata["いとこ"])
+		number = 10
+		return(kill_list[number]+"が亡くなったので、本日は休ませていただきます。")
+
+	else :
+#叔父、叔母をランダムで選ぶ
+		if int(userdata["叔母"]) and int(userdata["叔父"]) >= 1 :
+			rand_kill = random.randint(1,2)
+
+			if rand_kill == 1 :
+				userdata["叔母"] = int(userdata["叔母"]) - 1
+				userdata["叔母"] = str(userdata["叔母"])
+				number = 9
+				return(kill_list[number]+"が亡くなったので、本日は休ませていただきます。")
+
+			elif rand_kill == 2 :
+				userdata["叔父"] = int(userdata["叔父"]) - 1
+				userdata["叔父"] = str(userdata["叔父"])
+				number = 8
+				return(kill_list[number]+"が亡くなったので、本日は休ませていただきます。")
+		else :
+#叔父叔母個別
+			if int(userdata["叔母"]) >= 1 :
+				userdata["叔母"] = int(userdata["叔母"]) - 1
+				userdata["叔母"] = str(userdata["叔母"])
+				number = 9
+				return(kill_list[number]+"が亡くなったので、本日は休ませていただきます。")
+
+			elif int(userdata["叔父"]) >= 1 :
+				userdata["叔父"] = int(userdata["叔父"]) - 1
+				userdata["叔父"] = str(userdata["叔父"])
+				number = 8
+				return(kill_list[number]+"が亡くなったので、本日は休ませていただきます。")
+			else :
+#祖父、祖母をランダムで選ぶ
+				if int(userdata["祖母"]) and int(userdata["祖父"]) >= 1 :
+					rand_kill = random.randint(1,2)
+
+					if rand_kill == 1 :
+						userdata["祖母"] = int(userdata["祖母"]) - 1
+						userdata["祖母"] = str(userdata["祖母"])
+						number = 3
+						return(kill_list[number]+"が亡くなったので、本日は休ませていただきます。")
+
+					elif rand_kill == 2 :
+						userdata["祖父"] = int(userdata["祖父"]) - 1
+						userdata["祖父"] = str(userdata["祖父"])
+						number = 2
+						return(kill_list[number]+"が亡くなったので、本日は休ませていただきます。")
+
+				else :
+#祖父祖母個別
+					if int(userdata["祖母"]) >= 1 :
+						userdata["祖母"] = int(userdata["祖母"]) - 1
+						userdata["祖母"] = str(userdata["祖母"])
+						number = 3
+						return(kill_list[number]+"が亡くなったので、本日は休ませていただきます。")
+
+					elif int(userdata["祖父"]) >= 1 :
+						userdata["祖父"] = int(userdata["祖父"]) - 1
+						userdata["祖父"] = str(userdata["祖父"])
+						number = 2
+						return(kill_list[number]+"が亡くなったので、本日は休ませていただきます。")
+
+					else :
+#父、母をランダムで選ぶ
+						if int(userdata["母親"]) and int(userdata["父親"]) >= 1 :
+							rand_kill = random.randint(1,2)
+
+							if rand_kill == 1 :
+								userdata["母親"] = int(userdata["母親"]) - 1
+								userdata["母親"] = str(userdata["母親"])
+								number = 1
+								return(kill_list[number]+"が亡くなったので、本日は休ませていただきます。")
+
+							elif rand_kill == 2 :
+								userdata["父親"] = int(userdata["父親"]) - 1
+								userdata["父親"] = str(userdata["父親"])
+								number = 0
+								return(kill_list[number]+"が亡くなったので、本日は休ませていただきます。")
+
+						else :
+#父母個別
+							if int(userdata["母親"]) >= 1 :
+								userdata["母親"] = int(userdata["母親"]) - 1
+								userdata["母親"] = str(userdata["母親"])
+								number = 1
+								return(kill_list[number]+"が亡くなったので、本日は休ませていただきます。")
+
+							elif int(userdata["父親"]) >= 1 :
+								userdata["父親"] = int(userdata["父親"]) - 1
+								userdata["父親"] = str(userdata["父親"])
+								number = 0
+								return(kill_list[number]+"が亡くなったので、本日は休ませていただきます。")
+
+							else :
+								return("ERROR：親戚の残りがありません！")
+
+
+#print(userdata)
+	userdata0 = str(userdata)
+	userdata2.write(userdata0)
+	userdata2.close()
