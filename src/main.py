f898927045a3cb040edd2bfc0ca6128cb4c62424
th-05takahashi <tnk55397@st.yamagata-u.ac.jp>
2016-12-07 16:24:56 +0900

    変数の変更など

--- src/main.py
+++ src/main.py
@@ -6,29 +6,66 @@ import file_func
 
 while True:
     while True:
-        if file_func.get_trig() = True:
+        if file_func.get_trig() == True:
+            # トリガーを取得
+            trigger = file_func.get_trig()
             file_func.get_prof()
             break
 
     # ユーザ情報を読み込む
     userdata = eval(open("../data/user_profile/user_info.txt","r").read())
     
-    # 理由テキストの生成に成功するまでループする
-    text = None
-    while(text is None):
-        # 理由生成アルゴリズムをランダムに選択
-        algorithm = random.randint(1, 2)
-        # 注意報・警報データを用いた理由生成
-        if(algorithm == 1):
-            # 注意報・警報データを取得し、存在するならテキストを生成
-            text = get_warning.get_warning(userdata)
-    
-        # 定型文による理由生成(仮)
-        elif(algorithm == 2):
-            text = "風邪を引いたので休みます。"
-    
-    # 整形してSlackに投稿
-    postSlack.post_RestMessage(text, userdata)
-    
-    # Botが休み・遅刻を肯定するようなメッセージを投稿
-    postSlack.post_BotMessage(rest_flag, userdata) # rest_flagはbttnが押された時に生成されるファイルの中身（青（遅刻）:0休み：それ以外）
+    text = ""
+
+    # 緊急ボタン(赤：休み)
+    if(trigger == "red"):
+        text,restext = "一身上の都合により休ませていただきます。"
+        bottext = "了解です！お大事に！"
+
+    # 緊急ボタン(青：遅刻)
+    if(trigger == "blue"):
+        text,restext = "寝坊してしまい、遅刻します。急いで向かいます。",""
+        bottext = "了解しました。気をつけてね。"
+
+
+    # 天気を理由にして休む場合
+    if(trigger == "weather"):
+        # 注意報・警報データを取得し、存在するならテキストを生成
+        text,restext = get_warning.get_warning(userdata)   # slackのチャンネルに送られるテキスト,ユーザへのレスポンステキスト
+        bottext = ""                                       # botが肯定するためのテキスト
+
+    # 自分が風邪を引いて休む場合
+    elif(trigger == "own_cold"):
+        #text,restext = "風邪を引いたので休みます。"
+        bottext = ""
+
+    # 子どもが風邪を引いて休む場合
+    elif(trigger == "child_cold"):
+        #text,restext = "風邪を引いたので休みます。"
+        bottext = ""
+
+    # 親戚の不幸で休む場合
+    elif(trigger == "relative"):
+        #text,restext = "風邪を引いたので休みます。"
+        bottext = ""
+
+    # 自分が風邪を引いてしまった場合
+    elif(trigger == "twitter"):
+        #text,restext = "風邪を引いたので休みます。"
+        bottext = ""
+
+
+    # 理由生成に失敗した場合
+    if(text == ""):
+        postSlack.post_OwnSlack(restext,userdata)
+
+    # 理由生成に成功した場合
+    else:
+        # 整形してSlackに投稿
+        postSlack.post_RestMessage(text, userdata)
+
+        # Botが休み・遅刻を肯定するようなメッセージを投稿
+        postSlack.post_BotMessage(bottext, userdata)
+
+        #ユーザのslackにレスポンスを送信
+        postSlack.post_OwnSlack(restext,userdata)
