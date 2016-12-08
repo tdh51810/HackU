616cd5d453f8507ba9888e0570262d0b8bb6efab
txs33590 <txs33590@st.yamagata-u.ac.jp>
2016-12-08 11:45:03 +0900

    天気・気温の正規表現でline文の変形
    textの気温の範囲の変更
    
    textの天気のelseの文の変更

--- src/get_weather.py
+++ src/get_weather.py
@@ -2,7 +2,7 @@
 import urllib.request
 import random
 import datetime
-
+import re
 
 # ユーザ情報を入れると、そのユーザの住所として登録されている地域の気象情報を返す関数
 def get_weather(userdata):
@@ -11,7 +11,7 @@ def get_weather(userdata):
 
     #今日の天気の取得
     today = datetime.date.today()
-    weather = 2#random.randint(1,3)
+    weather = 1#random.randint(1,3)
    
     if(weather == 1):
        weather_text = get_temperture()
@@ -50,23 +50,29 @@ def get_temperture():
     else:
         area_id = address[1] + "0010"
     url = "http://weather.livedoor.com/forecast/rss/area/" + area_id + ".xml"
-    print(url)
     xml_temperture = urllib.request.urlopen(url).read().decode("utf-8").splitlines()
 
     # XMLデータの各行から、詳しい住所の気温データを検索
     temperture = []
     for line in xml_temperture:
       # 住所の気温が記載されている行を見つけたら、その行から気温部分のテキストのみを切り出す
-      if("<title>" in line and str(today.day)+"日" in line):
-        temperture = 18
+      if("<description>" in line and str(today.day)+"日" in line and "℃" in line):
+
+         if("-" in line):      
+            temperture = re.sub("<description>[0-3][0-9]",'',line)     
+            temperture = re.sub("[^-\d{1}]?",'',temperture)
+        
+         elif("-" not in line):
+            temperture = re.sub("<description>[0-3][0-9]",'',line)     
+            temperture = re.sub("[^-\d{1}]?",'',temperture)
               
-        if(temperture >= 25):
-           weather_text1 = "気温が"+str(temperture)+"度と高温ですので、\n熱中症予防として"
-        elif(temperture <= 0):
-           weather_text1 = "気温が"+str(temperture)+"度と低温ですので、\n凍結での転倒防止として"
-        elif(temperture < 25 and temperture >0):
-           weather_text1 = "気温が"+str(temperture)+"度と普通ですが、\n今日は、温度差が激しい日とTVで言っていたので、\n"
-        return weather_text1
+         if(int(temperture) >= 30):
+            weather_text1 = "気温が"+temperture+"度と高温ですので、\n熱中症予防として"
+         elif(int(temperture) <= 0):
+            weather_text1 = "気温が"+temperture+"度と低温ですので、\n凍結での転倒防止として"
+         elif(int(temperture) < 30 and int(temperture) >0):
+            weather_text1 = "気温が"+temperture+"度と普通ですが、\n今日は、温度差が激しい日とTVで言っていたので、\n"
+         return weather_text1
 
 
 def get_state():
@@ -95,17 +101,18 @@ def get_state():
     state = []
     for line in xml_state:
       # 住所の天気が記載されている行を見つけたら、その行から天気部分のテキストのみを切り出す
-      if("<title>" in line and "月"+str(today.day)+"日" in line):
-        
-        state = "yuki"
-        #state= line.split()         
-        #warning = line.split(" - ")[1].split("が発表されています")[0].split("、")
-     
+      if("<description>" in line and str(today.day)+"日" in line and "℃" in line):
+        #print(line)
+
+        state = re.sub("<description>[0-3][0-9]日（[^\d{1}]）の天気は",'',line)
+        state = re.sub("[\d{2}]",'',state)
+        state = re.sub("、[^0-9]*",'',state)
+
         if(state == "暴風雪" or state == "雨で暴風を伴う" or state == "雨" or state == "雪"):
           weather_text2 = "今日の天気が"+state+"ということで悪天候なので\n"
         else:
-          weather_text2 = "天気は今は大丈夫なのですが、今朝ご飯粒がお茶椀から綺麗にとれました。\nよって、先人の伝えを信じると雨の恐れがあるので、"
-          return weather_text2 
+          weather_text2 = "今の天気は"+state+"ということで大丈夫なのですが、今朝ご飯粒がお茶椀から綺麗にとれました。\nよって、先人の伝えを信じると雨の恐れがあるので、"
+        return weather_text2 
 
 
 def get_warning():
