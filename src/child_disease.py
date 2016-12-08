1b08198b1fc510a35d08a967c7d91996d35d6705
txs33590 <txs33590@st.yamagata-u.ac.jp>
2016-12-08 17:47:18 +0900

    text,restextの追加

--- src/child_disease.py
+++ src/child_disease.py
@@ -3,12 +3,14 @@ import random
 # 子供の病気を伝えて休む関数
 def child_disease(userdata):
     if(userdata["子ども"] != "0"):
-        diseases = ["を歯医者に連れていきます","が風邪をひいてしまった","が骨折をしてしまった","が熱を出してしまった","に蕁麻疹が出てしまった","感染症の疑いがあり病院へ連れていきます"]
+        diseases = ["を歯医者に連れていきます","が風邪をひいてしまった","が骨折をしてしまった","が熱を出してしまった","に蕁麻疹が出てしまった","が感染症の疑いがあり病院へ連れていきます"]
         childreason = random.choice(diseases)
         text = "おはようございます。\n申し訳ないのですが、子供" + childreason + "ので、今日はお休みをいただきます。"
-        restext = "ここにレスポンスの文章が入るよ"
-        return [text,restext]
+        restext = "送信が完了しました。"
+        #return [text,restext]
+
     else:
-        text = None
-        restext = "ここにエラーメッセージの文章が入るよ"
-        return [text,restext]
+        text = "None"
+        restext = "子供はいないので送信できませんでした。"
+            
+    return [text,restext]
