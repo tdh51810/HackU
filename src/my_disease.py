feb31deabe0d0fac8180e175d3e104011d91e2ea
tnk55397 <tnk55397@st.yamagata-u.ac.jp>
2016-12-09 14:55:31 +0900

    textの修正

--- src/my_disease.py
+++ src/my_disease.py
@@ -4,7 +4,7 @@ import random
 def my_disease(userdata):
     diseases = ["歯痛がひどく歯医者に行きます","ぎっくり腰になってしまった","風邪をひいてしまった","骨折をしてしまった","高熱が出てしまった","蕁麻疹が出てしまった","感染症の疑いがあり病院へ行きます"]
     myreason = random.choice(diseases)
-    text = "おはようございます。\n申し訳ないのですが、" + myreason + "ので、今日はお休みをいただきます。"
+    text = myreason + "ので、今日はお休みをいただきます。"
     restext = "送信が完了しました。"
     return [text,restext]
 
